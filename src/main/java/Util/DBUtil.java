52f64a5f639d462f8ddc26ca11b56ea3409a9022
blu3ter <simone.sc.catenaccio@gmail.com>
2024-06-18 14:15:53 +0200

    Initial commit

--- src/main/java/Util/DBUtil.java
+++ src/main/java/Util/DBUtil.java
@@ -1,27 +1,29 @@
 package Util;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import static java.lang.Class.forName;
+import java.sql.Statement;
 
 public class DBUtil {
-    private static final String URL = "jdbc:postgresql://localhost:5432/progetto_sg_oo_bdd?currentSchema=social_group";
+    private static final String URL = "jdbc:postgresql://localhost:5432/progetto_sg_oo_bdd";
     private static final String USER = "postgres";
     private static final String PASSWORD = "1234";
 
     public static Connection getConnection() throws SQLException {
+        Connection connection = null;
         try {
-            forName("org.postgresql.Driver");
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            try (Statement stmt = connection.createStatement()) {
+                stmt.execute("SET search_path TO social_group");
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             throw new SQLException(e);
         }
 
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return connection;
     }
 }
-
 
